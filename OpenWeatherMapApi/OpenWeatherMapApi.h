32c629c322a89fe128062ce86a4b3aacbcbd5139
Kwan Cheng <kwan.cheng@ghengiskwan.com>
2017-02-28 12:04:07 -0500

    Initial Commit

---
+++ OpenWeatherMapApi/OpenWeatherMapApi.h
@@ -0,0 +1,19 @@
+//
+//  OpenWeatherMapApi.h
+//  OpenWeatherMapApi
+//
+//  Created by Cheng, Yu Kwan on 2/28/17.
+//  Copyright © 2017 GhengisKwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenWeatherMapApi.
+FOUNDATION_EXPORT double OpenWeatherMapApiVersionNumber;
+
+//! Project version string for OpenWeatherMapApi.
+FOUNDATION_EXPORT const unsigned char OpenWeatherMapApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenWeatherMapApi/PublicHeader.h>
+
+
